bccc4905c238ebf5925d5247c4d0177446d44dbe
ryan.goss <ryan.goss@davita.com>
2025-05-16 15:28:53 -0500

    feat: added 60 fps

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    clock = pygame.time.Clock() 
+    dt = 0  # Initialize delta time
     print("Starting Asteroids!")
     print("Screen width:",SCREEN_WIDTH)
     print("Screen height:",SCREEN_HEIGHT)
@@ -14,12 +15,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                return
+                return 
 
         screen.fill("black")  # Fill the screen with black
         pygame.display.flip()  # Update the display
 
-    
+        dt = clock.tick(60) / 1000.0  # Calculate delta time
 
 if __name__ == "__main__":
     main()
