4b431dc786f45f95552f52d54ee31b43b317c646
ryan.goss <ryan.goss@davita.com>
2025-05-22 09:48:30 -0500

    feat: added collisions

--- circleshape.py
+++ circleshape.py
@@ -9,9 +9,9 @@ class CircleShape(pygame.sprite.Sprite):
         else:
             super().__init__()
 
-        self.position = pygame.Vector2(x, y)
-        self.velocity = pygame.Vector2(0, 0)
-        self.radius = radius
+        self.position = pygame.Vector2(x, y) # position of the object
+        self.velocity = pygame.Vector2(0, 0) # velocity of the object
+        self.radius = radius # radius of the object
 
     def draw(self, screen):
         # sub-classes must override
@@ -23,3 +23,6 @@ class CircleShape(pygame.sprite.Sprite):
         # sub-classes must override
         _ = dt
         pass
+
+    def collides_with(self, other):
+        return self.position.distance_to(other.position) <= self.radius + other.radius
